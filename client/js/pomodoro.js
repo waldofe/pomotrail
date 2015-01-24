e2810c6059e06b739263e8cdf5576230db7202bf
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2015-01-24 17:55:54 -0200

    renaming lib

---
+++ client/js/pomodoro.js
@@ -0,0 +1,60 @@
+if (Meteor.isClient) {
+  Clock = {
+    init: function (seconds) {
+      this.seconds = seconds;
+    },
+
+    actualMinutes: function () {
+      return Math.floor(this.seconds / 60);
+    },
+
+    actualSeconds: function () {
+      var realSeconds = (this.seconds % 60);
+
+      return (realSeconds < 10 ? realSeconds + '0' : realSeconds);
+    },
+
+    timer: function () {
+      return this.actualMinutes() + ':' + this.actualSeconds();
+    }
+  }
+
+  Pomodoro = {
+    initialize: function () {
+      this.totalSeconds = 1500;
+      Clock.init(this.totalSeconds);
+      Session.set("pomodoroTimer", Clock.timer());
+    },
+
+    play: function () {
+      var that = this;
+
+      timeLeft = function() {
+        if (that.totalSeconds > 0) {
+          that.totalSeconds--;
+
+          Clock.init(that.totalSeconds);
+
+          Session.set("pomodoroTimer", Clock.timer());
+        } else {
+          return Meteor.clearInterval(this.interval);
+        }
+      }
+
+      this.interval = Meteor.setInterval(timeLeft, 1000);
+
+      return true;
+    },
+
+    pause: function () {
+      Meteor.clearInterval(this.interval);
+      Session.set("pomodoroTimer", Clock.timer());
+
+      return true;
+    },
+
+    reset: function () {
+      this.initialize();
+    }
+  }
+}
