d9084756685644f98ebf7c92f6ddc013cdb647cb
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2015-01-24 01:23:57 -0200

    marking task as done

--- pomotrail.js
+++ pomotrail.js
@@ -51,9 +51,11 @@ if (Meteor.isClient) {
 
   Template.task.events({
     "click .toggle-checked": function () {
+      console.log(this._id);
       // Set the checked property to the opposite of its current value
       Tasks.update(this._id, {$set: {checked: ! this.checked}});
     },
+
     "click .delete": function () {
       Tasks.remove(this._id);
     }
