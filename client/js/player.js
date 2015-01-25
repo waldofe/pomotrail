c8e9effaa8104a261faa3fb770f92d8708586ede
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2015-01-24 22:14:26 -0200

    improving task layout

--- client/js/player.js
+++ client/js/player.js
@@ -7,17 +7,17 @@ if (Meteor.isClient) {
   });
 
   Template.body.events({
-    "click .controls .play": function (event) {
+    "click .pomodoro-player .controls .play": function (event) {
       Pomodoro.play();
       Session.set('playerMode', 'pause');
     },
 
-    "click .controls .pause": function (event) {
+    "click .pomodoro-player .controls .pause": function (event) {
       Pomodoro.pause();
       Session.set('playerMode', 'play');
     },
 
-    "click .controls .reset": function (event) {
+    "click .pomodoro-player .controls .reset": function (event) {
       Pomodoro.reset();
       Pomodoro.pause();
       Session.set('playerMode', 'play');
